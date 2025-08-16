e83ab5e08d93bec5d9aef4371c44a9934a2058ca
Kbomi16 <kimbomi2172@naver.com>
2025-08-16 21:47:09 +0900

    🎉 프로젝트 시작!

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+export default function home() {
+  return (
+    <div className="bg-primary">
+      HOME
+      <div className="relative inline-block overflow-hidden rounded">
+        <button className="btn-grad-green flex rounded-md via-[#43a047] p-4 text-white [--angle:135deg] [--end:#81c784] [--start:#2e7d32] hover:[--angle:160deg] hover:[--end:#2e7d32] hover:[--start:#81c784] focus:outline-none focus-visible:ring-2 focus-visible:ring-white/40">
+          버튼
+        </button>
+      </div>
+    </div>
+  )
+}
